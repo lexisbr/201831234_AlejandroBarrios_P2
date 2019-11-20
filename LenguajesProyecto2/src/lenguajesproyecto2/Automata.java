2269629c02268b911c82a070c36480ce449d1119
lexisbr <josebarriosrodas@gmail.com>
2019-11-20 16:04:18 -0600

    Tercer Commit

---
+++ LenguajesProyecto2/src/lenguajesproyecto2/Automata.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lenguajesproyecto2;
+
+/**
+ *
+ * @author alejandrobr
+ */
+public class Automata {
+     public void Automata(String documento){
+         String texto = documento;
+         System.out.println("");
+     }
+    
+}
