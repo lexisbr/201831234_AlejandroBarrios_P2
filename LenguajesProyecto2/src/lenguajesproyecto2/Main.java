27de67c356d09e2f372766283c9fa3ac99449cc8
lexisbr <josebarriosrodas@gmail.com>
2019-11-21 11:54:41 -0600

    Ultimo Commit

--- LenguajesProyecto2/src/lenguajesproyecto2/Main.java
+++ LenguajesProyecto2/src/lenguajesproyecto2/Main.java
@@ -105,8 +105,8 @@ public class Main extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-     Procesos misProcesos=new Procesos();
-  misProcesos.cargarArchivo();
+     InfoGramatica a = new InfoGramatica();
+     a.setVisible(true);
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
