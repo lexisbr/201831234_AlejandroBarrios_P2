654571495dd06fbac7f8b069dfb1507de4abca2c
lexisbr <josebarriosrodas@gmail.com>
2019-11-21 05:13:36 -0600

    Sexto Commit

--- LenguajesProyecto2/src/lenguajesproyecto2/Automata.java
+++ LenguajesProyecto2/src/lenguajesproyecto2/Automata.java
@@ -5,15 +5,45 @@
  */
 
 package lenguajesproyecto2;
+import java.util.StringTokenizer;
 
 /**
  *
  * @author alejandrobr
  */
 public class Automata {
+     String[] arraytokens;
+      Resultado a = new Resultado();
      public void Automata(String documento){
-         String texto = documento;
-         System.out.println("");
+         int i=0;
+        String nombre=documento;
+       
+	    StringTokenizer tokens=new StringTokenizer(nombre);
+            arraytokens = new String[tokens.countTokens()];
+          while(tokens.hasMoreTokens()){
+              arraytokens[i]=(tokens.nextToken());
+            i++;
+        }
+         
+         for (int j = 0; j < arraytokens.length; j++) {
+             System.out.println(arraytokens[j]);
+            
+         }
+         mostrarTokens();
+         a.setVisible(true);
      }
+     public void mostrarTokens(){
+          a.recibirTokens(arraytokens);
+     }
+     
+     public void analizarTokens(){
+         for (int i = 0; i < arraytokens.length; i++) {
+             
+         }
+     }
+         
+        
+         
+     
     
 }
